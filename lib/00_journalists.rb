466ec4a8dd9cb98699323c9c4804bbc4d7957cc7
Korblen <malo.bastianelli@gmail.com>
2024-01-18 13:27:02 +0100

    journalists

---
+++ lib/00_journalists.rb
@@ -0,0 +1,133 @@
+journ = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+menusse = 0
+def menu(menusse, e)
+    if menusse == 0
+        puts "Bienvenue ! Choisissez l'option que vous voulez parmi toutes nos magnifiques (mais très frustrantes a coder) fonctions"
+        menusse = menusse + 1
+    else 
+        system('clear')
+        puts "d'accord, y'a-t-il autre chose ?"
+    end
+    puts "option 1 : afficher l'array (1)"
+    puts "option 2 : nombre de handles dans l'array (2)"
+    puts "option 3 : donner le handle le plus court (3)"
+    puts "option 4 : donner le nombre de handle a 5 caractères (4)"
+    puts "option 5 : donner le nombre de handle commencant par une maj (5)"
+    puts "option 6 : renvoyer la liste triée par ordre alphabétique (6)"
+    puts "option 7 : renvoyer la liste triée par ordre croissant (7)"
+    puts "option 8 : renvoyer la position du handle \" \@epenser \" (8)"
+    puts "option 9 : trier les handles par ordre croissant et rendre plusieurs tableaux en fonction des nombres (si c'est bien ce qui est demandé ..?) (9)"
+    print ">"
+    option = gets.chomp.to_i
+    if option.is_a?(Integer) == false 
+        menu agressif
+    elsif option == 1
+        puts e
+    elsif option == 2
+        compteur = e.length
+        puts compteur
+    elsif option == 3
+        minimum = e.min_by(&:length)
+        puts minimum
+    elsif option == 4
+        konteur = nbr_cinq(e)
+        puts konteur
+    elsif option == 5
+        kontheur = check_capitale(e)
+        puts kontheur
+    elsif option == 6
+        aaaaaaaah = array_sort(e)
+        puts aaaaaaaah
+    elsif option == 7
+        array_de_merde = tri_nbr(e)
+        puts array_de_merde
+    elsif option == 8
+        position = position_handle(e, "@epenser")
+        if position
+            puts "Position de '@epenser': #{position}"
+        else
+            puts "'@epenser' non trouvé dans la liste."
+        end
+    elsif option == 9
+        groupes = tri_et_groupe_par_longueur(e)
+        groupes.each { |k, v| puts "Longueur #{k}: #{v}" }
+    else
+    end
+end
+
+def n_caracteres(e)
+    p = e.scan(/\w/)
+    return p.length
+end
+
+def n_caractere_max(e)
+    v = 0
+    for element in e
+        p = element.length
+        if v < p 
+            array_de_merde = tri_nbr(e)
+            v = p
+        end
+    end
+    return v
+end
+
+def array_sort(e)
+    trié_wsh = e.sort_by(&:downcase)
+    return trié_wsh
+end
+
+def tri_nbr(e)
+    trié_par_taille = e.sort_by(&:length)
+    return trié_par_taille
+end
+
+def capitale (e)
+    p = e.scan(/\w/).first
+    p.match?(/\A[A-Z]/)
+end
+
+def nbr_cinq(e)
+    konteur = 0
+    for element in e
+    if n_caracteres(element) == 5
+        konteur = konteur + 1
+    end
+    end
+    return konteur
+end
+
+def check_capitale(e)
+    kontheur = 0
+    for element in e
+    if capitale(element) == true 
+        kontheur = kontheur + 1
+    end
+    end
+    return kontheur
+end
+
+def position_handle(e, handle)
+    position = e.index(handle)
+    return position
+end
+
+def tri_et_groupe_par_longueur(e)
+    groupes = e.group_by { |handle| handle.length }
+    groupes.each { |k, v| groupes[k] = v.sort }
+    return groupes
+end
+
+menu(menusse, journ)
+
+loop do 
+    puts "retourner au menu ? (1= oui / 2 = non)"
+    menusse = menusse + 1
+    print ">"
+    retour = gets.chomp.to_i
+    if retour == 1
+        menu(menusse, journ)
+    else 
+        exit
+    end
+end
